33b6d03532490cda651cd6db31ef9e41182df0bf
Mayank Khanna <khanna98@outlook.com>
2018-06-15 20:02:40 +0530

    Update pattern

--- June 15/Ques 4/pattern.java
+++ June 15/Ques 4/pattern.java
@@ -4,8 +4,11 @@ public class pattern {
 	
 	public static void main(String[] args) {
 		for(int i=1;i<5;i++) {
-			for(int j=1;j<=i;j++) {
-				System.out.print((char)('a'+j-1));
+			for(int j=5;j>=i;j--) {
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++) {
+				System.out.print((char)('a'+k-1) + " ");
 			}
 			System.out.println();
 		}
